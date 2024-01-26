b504fd0674774a65e25532f91134828f92339d25
Xueguang <lu.xue@northeastern.edu>
2024-01-26 14:33:23 -0500

    re-arraged directories

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='marl_envs',
-    version='0.0.2',
+    version='0.0.3',
     description='Multi-agent Environments',
     author='Yuchen Xiao',
     author_email='xiao.yuch@husky.neu.edu',
